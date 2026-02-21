c0bf6e99b64b0466e21be59b2aa41dd123b00d24
Akshita Kalakonda <akshitha.2805@gmail.com>
2026-02-21 18:38:45 +0530

    Add files via upload

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,33 @@
+import pandas as pd
 
+def load_data(filepath):
+    df = pd.read_csv(filepath)
+    return df
+
+
+def preprocess_data(df):
+    # Selecting important features as per project document
+    selected_features = [
+        'DAY_OF_MONTH',
+        'DAY_OF_WEEK',
+        'OP_CARRIER_AIRLINE_ID',
+        'ORIGIN_AIRPORT_ID',
+        'DEST_AIRPORT_ID',
+        'DEP_TIME',
+        'ARR_TIME',
+        'DEP_DEL15',
+        'DIVERTED',
+        'DISTANCE',
+        'ARR_DEL15'
+    ]
+
+    df = df[selected_features]
+
+    # Remove missing values
+    df = df.dropna()
+
+    # Features and Label
+    X = df.drop('ARR_DEL15', axis=1)
+    y = df['ARR_DEL15']
+
+    return X, y
